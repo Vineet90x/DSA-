1070f3ee9a373d6ea9e6227684c8f392cc12fc4e
Vineet <vineetsalian42@gmail.com>
2026-08-19 00:02:50 +0530

    star pyramid

--- Patterns/Pattern_7.py
+++ Patterns/Pattern_7.py
@@ -1,1 +1,17 @@
-print(1)
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+
+def pattern(num):
+    for i in range(num//2+1):
+        for j in range (num):
+            if j >= num//2 - i and j <= num //2 + i:
+                print("*",end='')
+            else:
+                print(" ",end='')
+        print()
+        
+
+pattern(9)
